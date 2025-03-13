63d12b6cb69a0442e2de6a398e7b399b8919f626
nikita <nikita@gmail.com>
2025-03-14 00:19:11 +0530

    index file changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 const connectDB = async () =>{
     try{
-         // await mongoose.connect(process.env.MONGODB_URI);
-        await mongoose.connect("mongodb://localhost:27017/test");
+          await mongoose.connect(process.env.MONGODB_URI);
+        //await mongoose.connect("mongodb://localhost:27017/test");
         console.log('mongdb connected');
 
     }catch(error){
