4852052dbcb53d8bacf144223b5c81f847a852bc
nikita <nikita@gmail.com>
2025-03-14 23:01:08 +0530

    index filechanges

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,8 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT;
-app.listen(PORT, () =>
-  console.log(`Server is running on http://localhost:${PORT}`)
-);
+// app.listen(PORT, () =>
+//   console.log(`Server is running on http://localhost:${PORT}`)
+// );
+
+module.exports = app;
